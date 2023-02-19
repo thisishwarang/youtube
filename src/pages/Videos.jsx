932786cabd0386b66d62923873871359fd49e128
thisishwarang <imhwarang4@naver.com>
2023-02-19 22:35:33 +0900

    dependency injection

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,27 +1,31 @@
-import { useQuery } from '@tanstack/react-query';
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import FakeYoutube from '../api/fakeYoutube';
-import { search } from '../api/youtube';
-import VideoCard from './../components/VideoCard';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from '../components/VideoCard';
+import Youtube, { search } from '../api/youtube';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
-    const {keyword} = useParams();
-    const {isLoading, error, data: videos} = useQuery(
-        ['videos', keyword], ()=>{
-            const youtube = new FakeYoutube();
-            return youtube.search(keyword);
-        });
-    return (
-        <>
-            <div>
-                {isLoading && <p>Loading...</p>}
-                {error && <p>Something is wrong...</p>}
-                {videos && (<ul>
-                    {videos.map(video => <VideoCard key={video.id} video={video}/>)}
-                    </ul>)}
-            </div>
-        </>
-    );
-}
+  const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], () => youtube.search(keyword));
 
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😖</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
