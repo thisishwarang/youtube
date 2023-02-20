abdc96ebda3f112c17c797317c9f86d38a5803bc
thisishwarang <imhwarang4@naver.com>
2023-02-20 15:02:01 +0900

    related videos update

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -8,7 +8,23 @@ export default class Youtube {
     async search(keyword) {
         return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
     }
-    
+
+    async channelImageURL(id) {
+        return this.apiClient.channels({params: {part: 'snippet', id}})
+        .then(res => res.data.items[0].snippet.thumbnails.default.url);
+    }
+    async relatedVideos(id) {
+        return this.apiClient.search({
+            params: {
+                part: 'snippet',
+                maxResults: 25,
+                type: 'video',
+                relatedToVideoId: id,
+            },
+        })
+        .then((res) => res.data.items.map((item) => ({...item, id: item.id.videoId})))
+    }
+
     async #searchByKeyword(keyword) {
         return this.apiClient.search({
             params: {
@@ -18,8 +34,7 @@ export default class Youtube {
                 q: keyword,
             },
         })
-        .then((res) => res.data.items)
-        .then((items) => items.map((item) => ({...item, id: item.id.videoId})))
+        .then((res) => res.data.items.map((item) => ({...item, id: item.id.videoId})))
         }
     async #mostPopular() {
         return this.apiClient.videos({
